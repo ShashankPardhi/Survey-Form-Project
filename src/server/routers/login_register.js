a411693b56dfb65e2e1649ab7a2888add7317102
Sayonil Mitra <sayonilmitra@gmail.com>
2023-02-28 08:39:47 +0530

    Added validation for login, register

--- src/server/routers/login_register.js
+++ src/server/routers/login_register.js
@@ -8,6 +8,15 @@ loginRouter.use(express.json())
 loginRouter.post('/login', (req, res) => {
     let { email, password } = req.body
 
+    // validation of user input
+    if (email === '' || password === '') {
+        res.end("Email and password must be non empty")
+    } else if (password.length < 8) {
+        res.end("Password must have minimum 8 characters")
+    } else {
+        loginUser()
+    }
+
     async function loginUser() {
         let isDataCorrect = await userModel.findOne({
             email: email,
@@ -21,13 +30,23 @@ loginRouter.post('/login', (req, res) => {
         }
 
     }
-
-    loginUser()
 })
 
 // register new user
 loginRouter.post('/register', (req, res) => {
     let { username, email, phone, profession, password } = req.body
+
+    // validation of user input
+    if (username === '' || email === '' || password === '' || phone === ''
+        || profession === '' || password === '') {
+        res.end('All fields are mandatory')
+    } else if (password.length < 8) {
+        res.end("Password must have minimum 8 characters")
+    } else if (phone.length < 10) {
+        res.end("Phone number must have at least 10 digits")
+    } else {
+        addUser()
+    }
 
     async function addUser() {
         let newUser = new userModel({
